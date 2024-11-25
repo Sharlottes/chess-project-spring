1f40872eb11c779de9dd6b4cfd11b872720b1177
AppleJuice87 <mujin0305@naver.com>
2024-11-25 14:52:34 +0900

    first commit

---
+++ src/main/java/com/chessprojectspring/ChessProjectSpringApplication.java
@@ -0,0 +1,13 @@
+package com.chessprojectspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessProjectSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChessProjectSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chessprojectspring/ChessProjectSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chessprojectspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessProjectSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
