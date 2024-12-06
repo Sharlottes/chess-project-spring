47e4d7c2502e377717b2f5753bf78bbede8fa629
Sharlotte <aaa9810321@gmail.com>
2024-12-07 03:39:19 +0900

    give cors perm to localhost

--- src/main/java/com/chessprojectspring/ChessProjectSpringApplication.java
+++ src/main/java/com/chessprojectspring/ChessProjectSpringApplication.java
@@ -2,6 +2,9 @@ package com.chessprojectspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ChessProjectSpringApplication {
@@ -10,4 +13,15 @@ public class ChessProjectSpringApplication {
         SpringApplication.run(ChessProjectSpringApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
 }
